74ef0218fafe62306acb9cc785736e057695db76
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-09-09 11:31:29 +0700

    add file

---
+++ src/giaiptbac1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class giaiptbac1 {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+        if (a!=0) {
+            double answer = -b /a ;
+            System.out.println("Equation pass with x = " + answer);
+
+        }else {
+            if(b==0) {
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("No solution!");
+            }
+        }
+    }
+}
